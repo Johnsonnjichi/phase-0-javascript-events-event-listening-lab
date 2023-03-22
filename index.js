038189736ec3b612a0b85fb8582e7b899360e201
johnson <johnsonnjichi@gmail.com>
2023-03-22 17:54:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+// function addingEventListener() {
+
+
+// const input = document.getElementById('button');
+
+// input.addEventListener('click', function () {
+//     alert('I was clicked!') ;
+// });
+ 
+
+// }
+
 function addingEventListener() {
-}
+
+
+    const event = document.getElementById('button')
+    
+    event.addEventListener('click', function () {
+        alert('I was clicked!') 
+    });
+     
+    
+    }
+
+    // two ways of how to work it for my personal understanding.
